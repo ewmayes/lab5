f8087e3771d8c6ca559477638b9de64d4d862176
Everett Mayes <ewmayes@hotmail.com>
2018-01-26 08:56:00 -0500

    first commit

---
+++ Lab5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of side for a pair of dice.");
+            int Sides = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Roll the dice.");
+
+            // method for rolling the dice
+            randomNumber(Sides);
+        }
+
+
+        public static void randomNumber(int Sides)
+        {
+
+            Random random = new Random();
+
+            for (int i = 0; i < 2; i++)
+            {
+
+                int randomNumber = random.Next(0, Sides);
+                Console.WriteLine(randomNumber);
+
+            }
+        }
+    }
+}
